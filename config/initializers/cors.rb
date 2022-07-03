79182e63179d307c5958bb8cfe4ff97f3774b7a5
Augusto Hubert <augustohub@gmail.com>
2022-07-03 19:41:19 -0300

    Fixes CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*',
-    resource '*',
-    headers: :any,
-    methods: %i[get post put patch delete]
+    origins '*'
+    resource '*', headers: :any, methods: %i[get post put patch delete]
   end
 end
